e9407943aabe1f92618919832270389abf640b02
The Evisu project <47283629+TheEvisu@users.noreply.github.com>
2020-09-24 21:13:22 +0300

    Add files via upload

---
+++ precache-manifest.aeaa6b73bd2f8666c9ff37337a401bfc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5eb6149582cb5a0aeb516a971a0e14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe484755998e3ab10be",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9260b070c7ca4159c64c",
+    "url": "/static/css/main.8f3378c6.chunk.css"
+  },
+  {
+    "revision": "cfe484755998e3ab10be",
+    "url": "/static/js/2.94250454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94250454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9260b070c7ca4159c64c",
+    "url": "/static/js/main.9c54f16c.chunk.js"
+  },
+  {
+    "revision": "7a48a84578d2e7c9ec0f",
+    "url": "/static/js/runtime-main.afa7ceb7.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/static/media/polygon.cc54c19b.jpg"
+  }
+]);
